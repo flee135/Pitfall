2f22d1cf6f980270e07ba7b53b82a34b8be0d4c6
Felix Lee <felix.lee135@gmail.com>
2017-10-08 00:58:39 -0700

    Clean up code.

--- Game/Scripts/player.gd
+++ Game/Scripts/player.gd
@@ -6,13 +6,14 @@ extends RigidBody
 export var movement_speed = 5
 export var bomb_drop_delay = 1000
 
+var player_bomb_scn
 var next_bomb_drop = 0
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	player_bomb_scn = preload("res://Game/Scenes/Weapons/player_bomb.tscn")
 	set_fixed_process(true)
-	pass
 
 func _fixed_process(delta):
 	var move_vector = Vector3(0,0,0)
@@ -30,6 +31,6 @@ func _fixed_process(delta):
 	
 	if (Input.is_action_pressed("use_weapon") and OS.get_ticks_msec() > next_bomb_drop):
 		next_bomb_drop = OS.get_ticks_msec() + bomb_drop_delay
-		var bomb_node = preload("res://Game/Scenes/Weapons/player_bomb.tscn").instance()
+		var bomb_node = player_bomb_scn.instance()
 		bomb_node.set_translation(Vector3(get_translation().x, bomb_node.get_node("TestCube").get_scale().y, get_translation().z))
-		get_node("/root").add_child(bomb_node)
+		get_node("/root/Node").add_child(bomb_node)
