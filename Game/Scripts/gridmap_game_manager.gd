3ac42528a47b2b5cddfca5b3899a957f26a7ddc6
Felix Lee <felix.lee135@gmail.com>
2017-10-08 13:39:57 -0700

    Modified game script to spawn enemies.

--- Game/Scripts/gridmap_game_manager.gd
+++ Game/Scripts/gridmap_game_manager.gd
@@ -4,42 +4,74 @@ extends Node
 # var a = 2
 # var b = "textvar"
 export var player_spawn_position = Vector3(0,0,0)
+export var spawn_wait_time = 10
+export var include_basic = true
+export var include_pathfinder = true
+export var enemies_per_wave = 10
 
-var basic_enemy_scn
-var path_enemy_scn
+var map_max_z = 99
+var map_min_z = -map_max_z
+var map_max_x = 99
+var map_min_x = -map_max_x
+var next_spawn = 0
+var basic_enemy_scn = preload("res://Game/Scenes/Characters/basic_enemy.tscn")
+var path_enemy_scn = preload("res://Game/Scenes/Characters/navigation_enemy.tscn")
 var player_node
-var player_shape_node
+
+var monsters = []
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	var player_scn = preload("res://Game/Scenes/Characters/player.tscn")
 	var chibi_player_scn = preload("res://Game/Scenes/Characters/chibi_player.tscn")
-	basic_enemy_scn = preload("res://Game/Scenes/Characters/basic_enemy.tscn")
-	path_enemy_scn = preload("res://Game/Scenes/Characters/navigation_enemy.tscn")
 	
-#	player_node = player_scn.instance()
-#	add_child(player_node)
-#	player_node.global_translate(Vector3(0,5,0))
-#	player_shape_node = BoxShape.new()
-#	player_node.add_shape(player_shape_node)
+#	# Create list of available monsters
+	if include_basic:
+		monsters.append(basic_enemy_scn)
+	if include_pathfinder:
+		monsters.append(path_enemy_scn)
 
+	# Spawn players
 	player_node = chibi_player_scn.instance()
 	add_child(player_node)
 	player_node.global_translate(player_spawn_position)
-	
-	var enemy_node = basic_enemy_scn.instance()
-	add_child(enemy_node)
-	enemy_node.global_translate(Vector3(5,1,5))
-	
-	var path_enemy_node = path_enemy_scn.instance()
-	add_child(path_enemy_node)
-	path_enemy_node.global_translate(Vector3(10, 1, -10))
-	
+
+	spawn_enemies()
+	# Set a timer for futures spawns
+	var _timer = Timer.new()
+	add_child(_timer)
+
+	_timer.connect("timeout", self, "spawn_enemies")
+	_timer.set_wait_time(spawn_wait_time)
+	_timer.set_one_shot(false) # Make sure it loops
+	_timer.start()
+
 	set_process(true)
 
 func _process(delta):
-	# Enable key for debugging to disable collider.
-	if (Input.is_action_pressed("disable_collider")):
-		var pos = player_node.get_translation()
-		get_node("/root/Node/Navigation/GridMap").set_cell_item(int(round((pos.x+1)/2-1)),0,int(round((pos.z+1)/2-1)),-1)
+	pass
+#	if (OS.get_unix_time() > next_spawn):
+#		next_spawn = OS.get_unix_time() + spawn_wait_time
+#		spawn_enemies()
+	
+func spawn_enemies():
+	for i in range(0, enemies_per_wave):
+		randomize()
+		var enemy_index = randi() % monsters.size()
+		randomize()
+		var side = randi() % 4
+		
+		var enemy_node = monsters[enemy_index].instance()
+		add_child(enemy_node)
+		randomize() # For position on side of map
+		if side == 0: # Top
+			var pos = randf() * (map_max_x - map_min_x) - map_max_x
+			enemy_node.global_translate(Vector3(pos, 2, map_min_z))
+		elif side == 1: # Bottom
+			var pos = randf() * (map_max_x - map_min_x) - map_max_x
+			enemy_node.global_translate(Vector3(pos, 2, map_max_z))
+		elif side == 2: # Left
+			var pos = randf() * (map_max_z - map_min_z) - map_max_z
+			enemy_node.global_translate(Vector3(map_min_x, 2, pos))
+		elif side == 3: # Right
+			var pos = randf() * (map_max_z - map_min_z) - map_max_z
+			enemy_node.global_translate(Vector3(map_min_x, 2, pos))
