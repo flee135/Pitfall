544a7986bca0c423b92e9f24bc96bc1c16f86e08
Felix Lee <felix.lee135@gmail.com>
2017-10-07 21:20:46 -0700

    Smarter enemy that avoids the hole.

--- Game/Scripts/navigation_enemy.gd
+++ Game/Scripts/navigation_enemy.gd
@@ -3,19 +3,18 @@ extends RigidBody
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
 export var speed = 2
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	set_fixed_process(true)
+var path = []
 
+func _ready():
+	set_fixed_process(true)
+	
 func _fixed_process(delta):
 	# Check if under stage. If so, delete enemy.
 	if get_translation().y < -10:
 		queue_free()
-	
+
 	var players = get_tree().get_nodes_in_group("player_group")
 	# Find nearest player
 	var closest_dist = 10000
@@ -26,7 +25,11 @@ func _fixed_process(delta):
 			closest_dist = dist
 			closest_player = p
 
-	# Move towards closest player
 	if closest_player != null:
-		var direction = (closest_player.get_translation() - get_translation()).normalized()
-		global_translate(direction*speed*delta) 
+		print(get_translation(), " ", closest_player.get_translation())
+		path = get_node("/root/Node/Navigation").get_simple_path(
+			get_translation(),
+			closest_player.get_translation())
+		if path.size() > 1:
+			var direction = (path[1] - path[0]).normalized()
+			global_translate(direction*speed*delta) 
