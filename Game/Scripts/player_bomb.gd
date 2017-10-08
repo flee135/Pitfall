3648ebd1460fb7ab9b504d2353df8052ee872126
Jeremy <jeremyjiang27@gmail.com>
2017-10-08 01:48:22 -0700

    Variable blast radius for bomb based on discussed grid pattern.

--- Game/Scripts/player_bomb.gd
+++ Game/Scripts/player_bomb.gd
@@ -47,5 +47,20 @@ func _process(delta):
 	
 func explode():
 	var pos = get_translation()
-	get_node("/root/Node/Navigation/GridMap").set_cell_item(int(round((pos.x+1)/2-1)),0,int(round((pos.z+1)/2-1)),-1)
+	var center_cell_row = int(round((pos.x+1)/2-1))
+	var center_cell_col = int(round((pos.z+1)/2-1))
+	var max_cell_dist
+	
+	if (blast_radius == 0):
+		max_cell_dist = 0
+	elif (blast_radius == 1):
+		max_cell_dist = 1
+	else:
+		max_cell_dist = (blast_radius - 1) * 2
+	
+	for i in range(center_cell_row - blast_radius, center_cell_row + blast_radius + 1):
+		for j in range(center_cell_col - blast_radius, center_cell_col + blast_radius + 1):
+			if (abs(i - center_cell_row) + abs(j - center_cell_col) <= max_cell_dist):
+				get_node("/root/Node/Navigation/GridMap").set_cell_item(i, 0, j, -1)
+	
 	queue_free()
