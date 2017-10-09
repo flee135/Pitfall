61a985b0357ae300d23bb715c1c1d91b8115e9e2
Jeremy <jeremyjiang27@gmail.com>
2017-10-08 18:46:13 -0700

    Finished text on start screen and connected Play button to load 25x25_gridmap scene.

---
+++ Game/Scripts/start_screen.gd
@@ -0,0 +1,19 @@
+extends Control
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	get_node("StartScreen/CenterContainer/VBoxContainer/Button").connect("pressed", self, "play")
+	
+	set_process(true)
+	
+func _process(delta):
+	var size = get_viewport().get_rect().size
+	set_size(size)
+	
+func play():
+	get_tree().change_scene("res://Game/Scenes/Maps/25x25_gridmap.tscn")
